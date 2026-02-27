ed16a7ea6a9c9d3280ba532df0d1e7d7fcb7dabc
adorno-dev <adorno.dev@gmail.com>
2026-02-27 14:09:54 -0300

    Workaround

--- src/lib.rs
+++ src/lib.rs
@@ -233,7 +233,7 @@ pub fn get_worker() -> Worker {
         if let Some(worker) = &*w.borrow() {
             worker.clone()
         } else {
-            let worker = Worker::new("jswasm/sqlite3-worker1.js")
+            let worker = Worker::new("/jswasm/sqlite3-worker1.js")
                 .expect("failed to create worker");
             
             *w.borrow_mut() = Some(worker.clone());
