20b1363535e63f354bdd6bd918315aa98125589b
adorno-dev <adorno.dev@gmail.com>
2026-02-28 03:05:08 -0300

    Initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -303,7 +303,7 @@ pub fn get_worker() -> Worker {
         if let Some(worker) = &*w.borrow() {
             worker.clone()
         } else {
-            let worker = Worker::new("jswasm/sqlite3-worker1.mjs")
+            let worker = Worker::new("jswasm/sqlite3-worker1.js")
                 .expect("failed to create worker");
             
             *w.borrow_mut() = Some(worker.clone());
